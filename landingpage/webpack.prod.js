4ab4fcfa015a71b7e32fae53ecb6de5c1bc2db13
Till Ganster <tillganster@web.de>
2020-11-04 21:13:16 +0100

    zindex

--- landingpage/webpack.prod.js
+++ landingpage/webpack.prod.js
@@ -1,6 +1,17 @@
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const { merge } = require('webpack-merge');
- const common = require('./webpack.common.js');
+const common = require('./webpack.config.common.js');
 
- module.exports = merge(common, {
-   mode: 'production',
- });
+module.exports = merge(common, {
+  // Enable minification and tree-shaking
+  mode: 'production',
+  optimization: {
+    minimizer: [
+      new OptimizeCssAssetsPlugin({}),
+      new TerserPlugin({
+        extractComments: false,
+      }),
+    ],
+  },
+});
